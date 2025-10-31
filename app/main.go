dc9c7f2164b9df538884c9b46b5bd74343885fad
Abhishek Kumar <gwydionmaranio@gmail.com>
2025-11-01 00:30:26 +0530

    step 2

--- app/main.go
+++ app/main.go
@@ -22,9 +22,13 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	Connection, err := l.Accept()
+
+	Connection.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
 }
